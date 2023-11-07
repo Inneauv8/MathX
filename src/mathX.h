9037ad28a4228ad34b6d281fe7fef25465a96b93
Mathieu Durand <mathieu_durand@icloud.com>
2023-11-07 09:32:42 -0500

    First commit

---
+++ src/mathX.h
@@ -0,0 +1,30 @@
+// @file
+// Created by Mathieu Durand on 2023-10-27.
+//
+
+#include <math.h>
+#include "Arduino.h"
+
+#ifndef MATHX_H
+#define MATHX_H
+
+double sqDist(double x1, double y1, double x2, double y2);
+double sqDist(double x, double y);
+
+double dist(double x1, double y1, double x2, double y2);
+double dist(double x, double y);
+
+double map(double value, double minValue, double maxValue, double minResult, double maxResult);
+double map(double value, double maxValue, double maxResult);
+
+double clamp(double value, double minValue, double maxValue);
+
+double wrap(double value, double minValue, double maxValue);
+
+double smallestDifferenceInWrap(double value, double targetValue, int minValue, int maxValue);
+
+double smallestAngleDifference(double angle, double targetAngle);
+
+double sigmoid(float x, float offsetX = 0, float offsetY = 0, float width = 0, float height = 0);
+
+#endif //MATHX_H
